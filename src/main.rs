c47fb9b0f3a8884bb1d5dad3552e2f65aa8eab19
RustemB <bakirov.com@yandex.ru>
2020-07-15 01:52:28 +0500

    feature added: output to file code: refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,89 @@
-use clap::{App, Arg};
+use clap::{crate_version, App, Arg};
 use serde_json;
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{self, BufReader, Read},
     process,
 };
 
 fn main() {
     let cli = App::new("fe")
-        .version("0.1.1")
+        .version(crate_version!())
         .author("RustemB <bakirov.com@yandex.ru>")
-        .about("JSON (other in next versions) manipulator.")
+        .about("JSON (other in next versions) manipulator")
+        .version_short("v")
         .arg(
             Arg::with_name("input")
                 .short("i")
                 .long("input")
                 .takes_value(true)
                 .value_name("FILE")
-                .help("Input file to manipulate."),
+                .help("Input file to manipulate"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .value_name("FILE")
+                .help("Output file"),
         )
         .arg(
             Arg::with_name("uglify")
                 .short("u")
                 .long("uglify")
                 .takes_value(false)
-                .help("Uglify data."),
+                .help("Uglify data"),
         )
         .get_matches();
     let mut user_input = String::new();
     match cli.value_of("input") {
-        Some(f) => {
-            let file = File::open(f);
-            match file {
-                Ok(fi) => {
-                    let mut reader = BufReader::new(fi);
-                    if let Err(x) = reader.read_to_string(&mut user_input) {
-                        println!("Problem with reading file `{}': {}", f, x);
-                        process::exit(1);
-                    }
-                }
-                Err(_) => {
-                    println!("File `{}' not exist.", f);
+        Some(f) => match File::open(f) {
+            Ok(fi) => {
+                let mut reader = BufReader::new(fi);
+                if let Err(x) = reader.read_to_string(&mut user_input) {
+                    println!("Problem with reading file `{}': {}", f, x);
                     process::exit(1);
                 }
             }
-        }
-        None => match io::stdin().read_to_string(&mut user_input) {
-            Ok(_) => {}
-            Err(x) => {
-                println!("Something went wrong! {}", x);
+            _ => {
+                println!("File `{}' not exist.", f);
                 process::exit(1);
             }
         },
+        None => {
+            if let Err(x) = io::stdin().read_to_string(&mut user_input) {
+                println!("Something went wrong! {}", x);
+                process::exit(1);
+            }
+        }
     }
-    let v: serde_json::Value = match serde_json::from_str(&user_input) {
+    let parsed_data: serde_json::Value = match serde_json::from_str(&user_input) {
         Ok(n) => n,
         Err(x) => {
             println!("Something went wrong! {}", x);
             process::exit(1);
         }
     };
-    let output_data = if cli.is_present("uglify") {
-        match serde_json::to_string(&v) {
-            Ok(n) => n,
-            Err(x) => {
-                println!("Something went wrong: {}", x);
-                process::exit(1);
-            }
-        }
+    let method_to_print = if cli.is_present("uglify") {
+        serde_json::to_string
     } else {
-        match serde_json::to_string_pretty(&v) {
-            Ok(n) => n,
-            Err(x) => {
-                println!("Something went wrong: {}", x);
-                process::exit(1);
-            }
+        serde_json::to_string_pretty
+    };
+    let output_data = match method_to_print(&parsed_data) {
+        Ok(n) => n,
+        Err(x) => {
+            println!("Something went wrong: {}", x);
+            process::exit(1);
         }
     };
-    println!("{}", output_data);
+    match cli.value_of("output") {
+        Some(n) => {
+            if let Err(x) = fs::write(n, output_data) {
+                println!("Something went wrong: {}", x);
+                process::exit(1);
+            }
+        }
+        None => println!("{}", output_data),
+    }
 }
