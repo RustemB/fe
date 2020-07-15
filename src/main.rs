0fae7bdb64b7f521b71497e75d6f189d153647fb
RustemB <bakirov.com@yandex.ru>
2020-07-15 14:59:02 +0500

    code refactoring: replaced matching of Results with unwrap_or_else in some places;

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,63 @@ use std::{
 };
 
 fn main() {
-    let cli = App::new("fe")
+    let cli = gen_cli();
+
+    let mut user_input = String::new();
+    match cli.value_of("input") {
+        Some(f) => match File::open(f) {
+            Ok(fi) => {
+                let mut reader = BufReader::new(fi);
+                if let Err(x) = reader.read_to_string(&mut user_input) {
+                    println!("Problem with reading file `{}': {}", f, x);
+                    process::exit(1);
+                }
+            }
+            _ => {
+                println!("File `{}' not exist.", f);
+                process::exit(1);
+            }
+        },
+        None => {
+            if let Err(x) = io::stdin().read_to_string(&mut user_input) {
+                println!("Something went wrong! {}", x);
+                process::exit(1);
+            }
+        }
+    }
+
+    let parsed_data: serde_json::Value = serde_json::from_str(&user_input).unwrap_or_else(|x| {
+        println!("Something went wrong! {}", x);
+        process::exit(1);
+    });
+
+    let method_to_print = if cli.is_present("uglify") {
+        serde_json::to_string
+    } else {
+        serde_json::to_string_pretty
+    };
+
+    let output_data = method_to_print(&parsed_data).unwrap_or_else(|x| {
+        println!("Something went wrong: {}", x);
+        process::exit(1);
+    });
+
+    match cli.value_of("output") {
+        Some(n) => {
+            if let Err(x) = fs::write(n, output_data) {
+                println!("Something went wrong: {}", x);
+                process::exit(1);
+            }
+        }
+        None => println!("{}", output_data),
+    }
+}
+
+fn gen_cli() -> clap::ArgMatches<'static> {
+    App::new("fe")
         .version(crate_version!())
         .author("RustemB <bakirov.com@yandex.ru>")
         .about("JSON (other in next versions) manipulator")
-        .version_short("v")
         .arg(
             Arg::with_name("input")
                 .short("i")
@@ -35,55 +87,5 @@ fn main() {
                 .takes_value(false)
                 .help("Uglify data"),
         )
-        .get_matches();
-    let mut user_input = String::new();
-    match cli.value_of("input") {
-        Some(f) => match File::open(f) {
-            Ok(fi) => {
-                let mut reader = BufReader::new(fi);
-                if let Err(x) = reader.read_to_string(&mut user_input) {
-                    println!("Problem with reading file `{}': {}", f, x);
-                    process::exit(1);
-                }
-            }
-            _ => {
-                println!("File `{}' not exist.", f);
-                process::exit(1);
-            }
-        },
-        None => {
-            if let Err(x) = io::stdin().read_to_string(&mut user_input) {
-                println!("Something went wrong! {}", x);
-                process::exit(1);
-            }
-        }
-    }
-    let parsed_data: serde_json::Value = match serde_json::from_str(&user_input) {
-        Ok(n) => n,
-        Err(x) => {
-            println!("Something went wrong! {}", x);
-            process::exit(1);
-        }
-    };
-    let method_to_print = if cli.is_present("uglify") {
-        serde_json::to_string
-    } else {
-        serde_json::to_string_pretty
-    };
-    let output_data = match method_to_print(&parsed_data) {
-        Ok(n) => n,
-        Err(x) => {
-            println!("Something went wrong: {}", x);
-            process::exit(1);
-        }
-    };
-    match cli.value_of("output") {
-        Some(n) => {
-            if let Err(x) = fs::write(n, output_data) {
-                println!("Something went wrong: {}", x);
-                process::exit(1);
-            }
-        }
-        None => println!("{}", output_data),
-    }
+        .get_matches()
 }
